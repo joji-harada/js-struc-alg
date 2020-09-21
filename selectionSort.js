70bf6e31e33371e764cbc97038aab12cefd7d591
George Allison <49288165+joji-harada@users.noreply.github.com>
2020-09-21 16:28:18 -0700

    selection sort

---
+++ selectionSort.js
@@ -0,0 +1,36 @@
+// Sort the array by selecting min val and placing it at beginning of arr
+/*
+    loop i will start at arr[0] and incrememnt up++
+        set min to arr[i], minIdx to i
+        inner loop j will compare agains arr[i]
+            store/update min
+            swap arr[i] with min if it is lower. (compare num val)
+    return arr
+*/
+
+function selectionSort(arr){
+    let min, minIdx;
+
+    const swap = (arr, i1, i2) => {
+        [arr[i1], arr[i2]] = [arr[i2], arr[i1]];
+    };
+
+    for(let i = 0; i < arr.length; i++){
+        minIdx = i;
+        min = arr[i];
+
+        for(let j = i + 1; j < arr.length; j++){
+            if(arr[j] < min) {
+                min = arr[j];
+                minIdx = j;
+            };
+        }
+
+        if(min < arr[i]) swap(arr, i, minIdx);
+    }
+
+    console.log(arr);
+    return arr;
+}
+
+selectionSort([8,2,5,9,1,11,3,7]);
