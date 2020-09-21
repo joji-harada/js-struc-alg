6be4ff5dc8d4939fb61a806fd0612eac16756347
George Allison <49288165+joji-harada@users.noreply.github.com>
2020-09-21 15:49:56 -0700

    added noSwap catch to break loop if already sorted

--- bubbleSort.js
+++ bubbleSort.js
@@ -1,18 +1,23 @@
 
 function bubbleSort(arr) {
+    let noSwaps;
     const swap = (arr, i1, i2) => {
         [arr[i1], arr[i2]] = [arr[i2], arr[i1]];
     };
 
     for (let i = arr.length; i > 0; i--){
+        noSwaps = true;
         for(let j = 0; j < i - 1; j++){
             if (arr[j] > arr[j + 1]){
                 swap(arr, j, j + 1);
+                noSwaps = false;
             }
         }
+        if(noSwaps) break;
     }
+    
     return arr;
 }
 
-bubbleSort([14,2,15,6]);
+bubbleSort([8,1,2,3,4,5,6,7]);
 
