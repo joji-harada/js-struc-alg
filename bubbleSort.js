2f00c30f9b3463b8c4e5a05a96658faf54520ebe
George Allison <49288165+joji-harada@users.noreply.github.com>
2020-09-21 15:41:33 -0700

    search/sort

---
+++ bubbleSort.js
@@ -0,0 +1,17 @@
+const swap = (arr, i1, i2) => {
+    [arr[i1], arr[i2]] = [arr[i2], arr[i1]];
+};
+
+const bubbleSort = (arr) => {
+    for (let i = arr.length; i > 0; i--){
+        for(let j = 0; j < i - 1; j++){
+            if (arr[j] > arr[j + 1]){
+                swap(arr, j, j + 1);
+            }
+        }
+    }
+    return arr;
+}
+
+bubbleSort([14,2,15,6]);
+
