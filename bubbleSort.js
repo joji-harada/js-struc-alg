2bb3933d4f11a2dbba6f68e09ccf75662b9b7d86
George Allison <49288165+joji-harada@users.noreply.github.com>
2020-09-21 15:44:05 -0700

    edit

--- bubbleSort.js
+++ bubbleSort.js
@@ -3,7 +3,7 @@ function bubbleSort(arr) {
     const swap = (arr, i1, i2) => {
         [arr[i1], arr[i2]] = [arr[i2], arr[i1]];
     };
-    
+
     for (let i = arr.length; i > 0; i--){
         for(let j = 0; j < i - 1; j++){
             if (arr[j] > arr[j + 1]){
