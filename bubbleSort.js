e0bd95d08ca523cbee33aeea69e9014092525a6b
George Allison <49288165+joji-harada@users.noreply.github.com>
2020-09-21 15:43:27 -0700

    scope edit

--- bubbleSort.js
+++ bubbleSort.js
@@ -1,8 +1,9 @@
-const swap = (arr, i1, i2) => {
-    [arr[i1], arr[i2]] = [arr[i2], arr[i1]];
-};
 
-const bubbleSort = (arr) => {
+function bubbleSort(arr) {
+    const swap = (arr, i1, i2) => {
+        [arr[i1], arr[i2]] = [arr[i2], arr[i1]];
+    };
+    
     for (let i = arr.length; i > 0; i--){
         for(let j = 0; j < i - 1; j++){
             if (arr[j] > arr[j + 1]){
