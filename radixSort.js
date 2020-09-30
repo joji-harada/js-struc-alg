3f0539905f08bed479937f4bb6a4c05e3aa86154
George Allison <49288165+joji-harada@users.noreply.github.com>
2020-09-30 15:36:39 -0700

    helper method typo (Math.abs) & radixSort complete

--- radixSort.js
+++ radixSort.js
@@ -8,11 +8,11 @@
     Third helper method to return the count of the number with 
     the most digits in a list of nums.
 
-    
+
 */
 
 function getDigit(num, i){
-    return Math.floor(math.abs(num) / Math.pow(10, i)) % 10;
+    return Math.floor(Math.abs(num) / Math.pow(10, i)) % 10;
 }
 
 function digitCount(num){
@@ -27,3 +27,18 @@ function mostDigits(nums){
     }
     return maxDigits;
 }
+
+function radixSort(nums){
+    let maxDigitCount = mostDigits(nums);
+    for(let k = 0; k < maxDigitCount; k++){
+        let digitBuckets = Array.from({length: 10}, () => []);
+        for(let i = 0; i < nums.length; i++){
+            let digit = getDigit(nums[i], k);
+            digitBuckets[digit].push(nums[i]);
+        }
+        nums = [].concat(...digitBuckets);
+    }
+    return nums;
+}
+
+console.log(radixSort([13,5145,64,43453,43]));
