d1c5d16dde1233664b20564da5f954aa9c7d0ced
George Allison <49288165+joji-harada@users.noreply.github.com>
2020-10-14 16:52:32 -0700

    doubly linked list. classes created & added push()

---
+++ dataStructures/Doubly_Linked_List.js
@@ -0,0 +1,29 @@
+class newNode {
+    constructor(val){
+        this.data = val;
+        this.prev = null;
+        this.next = null;
+    }
+}
+
+class DoublyLinkedList {
+    constructor(){
+        this.head = null;
+        this.tail = null;
+        this.length = 0;
+    }
+    
+    push(val){
+        let newNode = new Node(val);
+        if(!this.head){
+            this.head = newNode;
+            this.tail = newNode;
+        } else {
+            this.tail.next = newNode;
+            newNode.prev = this.tail;
+            this.tail = newNode;
+        }
+        this.length++;
+        return this;
+    }
+}
