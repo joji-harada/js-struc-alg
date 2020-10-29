16f0e51ccd2c776f049cd099761204450a28144e
George Allison <49288165+joji-harada@users.noreply.github.com>
2020-10-29 16:59:30 -0700

    underlying data structure for stack

---
+++ dataStructures/Stack.js
@@ -0,0 +1,22 @@
+class Node{
+    constructor(val){
+        this.val = val;
+        this.next = null;
+    }
+}
+class Stack {
+    constructor(){
+        this.first = null;
+        this.last = null;
+        this.size = 0;
+    }
+
+    push(val){
+        
+    }
+
+    pop(){
+
+    }
+}
+
