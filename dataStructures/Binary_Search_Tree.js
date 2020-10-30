4ba6d5ce46812e8b2ddbb1c6862683bcbe8aba1b
George Allison <49288165+joji-harada@users.noreply.github.com>
2020-10-30 14:26:54 -0700

    BTS breadth first search (traversal)

--- dataStructures/Binary_Search_Tree.js
+++ dataStructures/Binary_Search_Tree.js
@@ -61,6 +61,21 @@ class BinarySearchTree {
             }
         }
     }
+    
+    bfs(){ //breadth first
+        let data = [],
+            queue = [],
+            node = this.root;
+
+        queue.push(node);
+        while(queue.length){
+            node = queue.shift();
+            data.push(node);
+            if(node.left) queue.push(node.left);
+            if(node.right) queue.push(node.right);
+        }
+        return data;
+    }
 }
 
 let tree = new BinarySearchTree();
