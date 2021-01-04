e4dbbe9825d03584b5d86f36639ce7d96fb68019
George Allison <49288165+joji-harada@users.noreply.github.com>
2021-01-04 15:28:28 -0800

    depthfirstrecursive implemented

--- dataStructures/Graph.js
+++ dataStructures/Graph.js
@@ -36,16 +36,43 @@ class Graph {
         }
         delete this.adjacencyList[vert];
     }
+
+    dfsRecursive(start){
+        let result = [];
+        let visited = {};
+        const adjacencyList = this.adjacencyList;
+
+        (function dfs(vertex){
+            if(!adjacencyList[vertex]) return undefined;
+            visited[vertex] = true;
+            result.push(vertex);
+            adjacencyList[vertex].forEach(neighbor => {
+                if(!visited[neighbor]){
+                    return dfs(neighbor);
+                }
+            });
+        })(start);
+
+        return result;
+    }
 }
 
 
 //for testing
 let g = new Graph;
 
-g.addVertex("Dallas");
-g.addVertex("Tokyo");
-g.addVertex("Aspen");
-g.addVertex("Seattle");
+g.addVertex("A");
+g.addVertex("B");
+g.addVertex("C");
+g.addVertex("D");
+g.addVertex("E");
+g.addVertex("F");
 
-g.addEdge("Tokyo", "Seattle");
-g.addEdge("Tokyo", "Aspen");
+g.addEdge("A", "B");
+g.addEdge("A", "C");
+g.addEdge("B", "D");
+g.addEdge("C", "E");
+g.addEdge("D", "E");
+g.addEdge("E", "F");
+
+console.log(g.dfsRecursive("B"));
