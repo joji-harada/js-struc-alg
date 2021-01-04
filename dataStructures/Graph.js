e76730a3fd6c928c69be97d3828a56361f1f28b4
George Allison <49288165+joji-harada@users.noreply.github.com>
2021-01-04 12:48:00 -0800

    Adjacency list - graph class. add vertex/edge functions.

---
+++ dataStructures/Graph.js
@@ -0,0 +1,22 @@
+class Graph {
+    constructor(){
+        this.adjacencyList = {};
+    }
+
+    addVertex(name){
+        if(!this.adjacencyList[name]) this.adjacencyList[name] = [];
+    }
+
+    addEdge(vert1, vert2){
+        if(this.adjacencyList[vert1] && this.adjacencyList[vert2]){
+            this.adjacencyList[vert1].push(vert2);
+            this.adjacencyList[vert2].push(vert1);
+        }
+    }
+}
+
+let g = new Graph;
+
+g.addVertex("Dallas");
+g.addVertex("Tokyo");
+g.addVertex("Aspen");
