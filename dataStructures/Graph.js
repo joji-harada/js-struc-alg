db1f2c8c5c6921568b9d5e52aa1d0f12226328e1
George Allison <49288165+joji-harada@users.noreply.github.com>
2021-01-04 12:59:34 -0800

    removeEdge implemented

--- dataStructures/Graph.js
+++ dataStructures/Graph.js
@@ -12,11 +12,22 @@ class Graph {
             this.adjacencyList[vert1].push(vert2);
             this.adjacencyList[vert2].push(vert1);
         } else {
-            console.log("One or more vertices don't exist.")
+            console.log("ERROR: One or more vertices don't exist.")
         }
     }
 
-
+    removeEdge(vert1, vert2){
+        if(this.adjacencyList[vert1] && this.adjacencyList[vert2]){
+            this.adjacencyList[vert1] = this.adjacencyList[vert1].filter(
+                v => v !== vert2
+            );
+            this.adjacencyList[vert2] = this.adjacencyList[vert2].filter(
+                v => v !== vert1
+            );
+        } else {
+            console.log("ERROR: One or more vertices don't exist.")
+        }
+    }
 }
 
 let g = new Graph;
@@ -24,3 +35,7 @@ let g = new Graph;
 g.addVertex("Dallas");
 g.addVertex("Tokyo");
 g.addVertex("Aspen");
+g.addVertex("Seattle");
+
+g.addEdge("Tokyo", "Seattle");
+g.addEdge("Tokyo", "Aspen");
