85ed3022cadb07e528463d7e97c2ccce8c01899a
George Allison <49288165+joji-harada@users.noreply.github.com>
2021-01-04 17:24:29 -0800

    updated priorityqueue, shortened runtime

--- dataStructures/Weighted_Graph.js
+++ dataStructures/Weighted_Graph.js
@@ -1,22 +1,3 @@
-class PriorityQueue { //simple PQ
-    constructor(){
-        this.values = [];
-    }
-
-    enqueue(val, priority){
-        this.values.push({val, priority});
-        this.sort();
-    }
-
-    dequeue(){
-        return this.values.shift();        
-    }
-
-    sort(){
-        this.values.sort((a, b) => a.priority - b.priority);
-    }
-}
-
 class WeightedGraph{
     constructor(){
         this.adjacencyList = {};
@@ -77,6 +58,82 @@ class WeightedGraph{
             }
         }
         return path.concat(smallest).reverse();
+    }
+}
+
+class Node {
+    constructor(val, priority){
+        this.val = val;
+        this.priority = priority;
+    }
+}
+
+class PriorityQueue {
+    constructor(){
+        this.values = [];
+    }
+    
+    enqueue(val, priority){
+        let newNode = new Node(val, priority);
+        this.values.push(newNode);
+        this.bubbleUp();
+    }
+
+    bubbleUp(){
+        let idx = this.values.length - 1;
+        const element = this.values[idx];
+        while(idx > 0){
+            let parentIdx = Math.floor((idx - 1) / 2);
+            let parent = this.values[parentIdx];
+            if(element.priority >= parent.priority) break;
+            this.values[parentIdx] = element;
+            this.values[idx] = parent;
+            idx = parentIdx;
+        }
+    }
+
+    dequeue(){
+        let min = this.values[0];
+        let end = this.values.pop();
+        
+        if(this.values.length > 0){
+            this.values[0] = end;
+            this.sinkDown();
+        }
+        
+        return min;
+    }
+
+    sinkDown(){
+        let idx = 0;
+        let length = this.values.length;
+        let element = this.values[0];
+        while(true){
+            let leftChildIdx = 2 * idx + 1;
+            let rightChildIdx = 2 * idx + 2;
+            let leftChild, rightChild;
+            let swap = null;
+
+            if(leftChildIdx < length){
+                leftChild = this.values[leftChildIdx];
+                if(leftChild.priority < element.priority){
+                    swap = leftChildIdx;
+                }
+            }
+            if(rightChildIdx < length){
+                rightChild = this.values[rightChildIdx];
+                if(
+                    (swap === null && rightChild.priority < element.priority) || 
+                    (swap !== null && rightChild.priority < leftChild.priority)
+                ) {
+                    swap = rightChildIdx;
+                }
+            }
+            if(swap === null) break;
+            this.values[idx] = this.values[swap];
+            this.values[swap] = element;
+            idx = swap;
+        }
     }
 }
 
