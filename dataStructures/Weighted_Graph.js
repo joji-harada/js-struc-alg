9957beaae2bb0be46ce62e1684b791d99b679653
George Allison <49288165+joji-harada@users.noreply.github.com>
2021-01-04 16:58:07 -0800

    Simple PQ class. Test implementations

--- dataStructures/Weighted_Graph.js
+++ dataStructures/Weighted_Graph.js
@@ -1,3 +1,22 @@
+class PriorityQueue { //simple PQ
+    constructor(){
+        this.values = [];
+    }
+
+    enqueue(val, priority){
+        this.values.push({val, priority});
+        this.sort();
+    }
+
+    dequeue(){
+        return this.values.shift();        
+    }
+
+    sort(){
+        this.values.sort((a, b) => a.priority - b.priority);
+    }
+}
+
 class WeightedGraph{
     constructor(){
         this.adjacencyList = {};
@@ -16,3 +35,22 @@ class WeightedGraph{
         }
     }
 }
+
+
+//FOR TESTING
+let g = new WeightedGraph();
+g.addVertex("A");
+g.addVertex("B");
+g.addVertex("C");
+g.addVertex("D");
+g.addVertex("E");
+g.addVertex("F");
+
+g.addEdge("A", "B", 4);
+g.addEdge("A", "C", 2);
+g.addEdge("B", "E", 3);
+g.addEdge("C", "D", 2);
+g.addEdge("C", "F", 4);
+g.addEdge("D", "E", 3);
+g.addEdge("D", "F", 1);
+g.addEdge("E", "F", 1);
