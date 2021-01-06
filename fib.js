f9485dc8b546a0029985e2d9e09c3208146211f4
George Allison <49288165+joji-harada@users.noreply.github.com>
2021-01-06 13:48:35 -0800

    fib with tabulation (no recursion)

--- fib.js
+++ fib.js
@@ -1,13 +1,22 @@
 //fibonaci implementation
 
-function fib(num, memo = []){
+function fibMemo(num, memo = []){
     if(memo[num] !== undefined) return memo[num];
     if (num <= 2) return 1;
-    let result = fib(num - 1, memo) + fib(num - 2, memo);
+    let result = fibMemo(num - 1, memo) + fibMemo(num - 2, memo);
     memo[num] = result;
     return result;
 }
 
+function fibTabulation(n){
+    if(n <= 2) return 1;
+    let fibNums = [0,1,1];
+    for(let i = 3; i <= n; i++){
+        fibNums[i] = fibNums[i - 1] + fibNums[i - 2];
+    }
+    return fibNums[n];
+}
 
 
-console.log(fib(100));
+console.log(fibMemo(100));
+console.log(fibTabulation(100));
