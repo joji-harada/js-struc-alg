cd0f9c3460cf4c0efdccd9820a9c136e702ce139
George Allison <49288165+joji-harada@users.noreply.github.com>
2021-01-06 13:34:10 -0800

    corrected recursive call params

--- fib.js
+++ fib.js
@@ -2,10 +2,12 @@
 
 function fib(num, memo = []){
     if(memo[num] !== undefined) return memo[num];
-    if (num <= 1) return num;
-    let result = fib(num - 1) + fib(num - 2);
+    if (num <= 2) return 1;
+    let result = fib(num - 1, memo) + fib(num - 2, memo);
     memo[num] = result;
     return result;
 }
 
 //BIG O IS 2^N
+
+console.log(fib(100));
