68fc046f2d4a84410900414ec286857938263a80
George Allison <49288165+joji-harada@users.noreply.github.com>
2021-01-06 13:28:43 -0800

    added memoization to fib

--- fib.js
+++ fib.js
@@ -1,9 +1,11 @@
 //fibonaci implementation
 
-function fib(num){
+function fib(num, memo = []){
+    if(memo[num] !== undefined) return memo[num];
     if (num <= 1) return num;
-
-    return fib(num - 1) + fib(num - 2);
+    let result = fib(num - 1) + fib(num - 2);
+    memo[num] = result;
+    return result;
 }
 
 //BIG O IS 2^N
